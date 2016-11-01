de028e110b6b6fd769fdaa392ede9fe9bd99ddfd
hector <hectordediego89@gmail.com>
2016-11-01 12:07:06 +0100

    Creando contacto

--- app/src/main/java/master/android/agenda/CreateActivity.java
+++ app/src/main/java/master/android/agenda/CreateActivity.java
@@ -2,14 +2,48 @@ package master.android.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class CreateActivity extends AppCompatActivity {
+
+    private EditText nombre;
+    private EditText apellidos;
+    private EditText telefono;
+    private EditText correo;
+    private EditText direccion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinnerTipo);
 
+        ArrayAdapter<Tipo> adapter = new ArrayAdapter<Tipo>(this, android.R.layout.simple_spinner_item, Tipo.values());
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+
+        mySpinner.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
